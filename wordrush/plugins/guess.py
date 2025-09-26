e2c55c4f3ad93522b732090b1f2b1fa8ce7a8a28
initAlishan <initalishan@gmail.com>
2025-09-26 19:11:10 +0530

    fix

--- wordrush/plugins/guess.py
+++ wordrush/plugins/guess.py
@@ -10,7 +10,7 @@ with open("word.txt") as f:
     
 @wordrush.on(events.NewMessage)
 async def guess(event):
-    if not event.is_private and not event.is_reply:
+    if not event.is_private and event.is_reply:
         return
     chat_id = event.chat_id
     if not chat_id in is_playing:
