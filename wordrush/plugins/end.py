e7e06dc3b1ae0b4c7ad34f4e5031713abc3cecc7
initAlishan <initalishan@gmail.com>
2025-09-26 16:11:04 +0530

    guess plugin

--- wordrush/plugins/end.py
+++ wordrush/plugins/end.py
@@ -7,7 +7,7 @@ from wordrush.config import is_playing
 async def game_end(event):
     chat_id = event.chat_id
     if not is_playing[chat_id]:
-        await event.respond("There is no game in **progress.**)
+        await event.respond("There is no game in **progress.**")
     else:
         await event.respond(f"Game ended!\nCorrect word was **{is_playing[chat_id]}**\nStart new game with **/new**")
         del is_playing[chat_id]
